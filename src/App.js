209093bebe7edce871e08d75da46f62ea6f21d4e
Ander Salas <ander.s.zabaleta@hotmail.com>
2020-11-24 20:54:55 +0100

    first Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+//importamos el array SistemaSolar
+
+import sistemaSolar from "./sistemaSolar";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  //Creamos la constante planetas y la igualamos a lo que nos devuelva la funcion .map()
+  //A la función .map le pasamos por parametros la variable planeta (o cualquier otro nombre), que nos servirá como puerta de acceso a cada elemento del array
+  const planetas = sistemaSolar.map((planeta) => {
+    //Devolvemos el código JSX (muy parecido al HTML) que queremos mostrar
+    return (
+      <div>
+        <h1>{planeta.nombre}</h1>
+        <p>{planeta.temperatura}</p>
+        <p>{planeta.color}</p>
+        <img src={planeta.imagen} alt="" />
+      </div>
+    );
+  });
+
+  //La función App devuelve la variable donde hemos guardado el código JSX
+  return planetas;
 }
 
 export default App;
